77089f9465f6c39398df91732c463c5528982dd2
ronaly <ronalyagdeppa@gmail.com>
2026-01-21 11:29:23 +0800

    add the query

--- sql/analysis/ebitda_vs_operating_income.sql
+++ sql/analysis/ebitda_vs_operating_income.sql
@@ -9,3 +9,12 @@ Question: How large is the gap between operating income and EBITDA?
 -- Insigt Angle: Depreciation Intensity /
 ---              Asset-heavy operations
 -- --------------------------------------------------------
+SELECT
+    d.fiscal_year,
+    f.operating_income,
+    f.ebitda,
+    f.ebitda - f.operating_income AS non_cash_adjustments
+FROM fact_financials f
+JOIN dim_date d ON f.date_key = d.date_key
+WHERE f.ebitda IS NOT NULL
+ORDER BY d.fiscal_year;
